94cd39151fe60d0497c96708c42a960ac945edd2
Christopher Wilkinson <git@wilk.tech>
2020-11-02 19:59:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function play() {
+    const overlay = document.querySelector('.overlay')
+    overlay.style.opacity = 1
+}
+
+function stop() {
+    const overlay = document.querySelector('.overlay')
+    overlay.style.opacity = 0
+}
+
+play()
