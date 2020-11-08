89a125847318c483efbb5e7414471b0d4a0126f4
Christopher Wilkinson <git@wilk.tech>
2020-11-08 14:49:46 +0000

    Some animation

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 function play() {
     const overlay = document.querySelector('.overlay')
     overlay.style.opacity = 1
+
+    let delay = 100
+    let line = document.querySelector('.line')
+
+    setTimeout(function() {
+        line.classList.add('show')
+    }, delay)
+
+
 }
 
 function stop() {
     const overlay = document.querySelector('.overlay')
     overlay.style.opacity = 0
+
+    let line = document.querySelector('.line')
+    line.classList.remove('show')
 }
 
 play()
